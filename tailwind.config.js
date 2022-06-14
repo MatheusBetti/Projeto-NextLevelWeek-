dc2846432508056e2ef6653c33bf5b16a0b51463
Matheus Betti <matheusbbetti@gmail.com>
2022-06-14 11:09:41 -0300

    .

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brand:{
+          500:'#8257e6',
+        }
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
+}
